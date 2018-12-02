7eeb7a42d1457e74e00965b2c6c19873da2948b2
Will Strinz <wstrinz@gmail.com>
2018-12-02 13:39:27 -0600

    day 2 part 2

--- lib/advent_of_code/day_2.ex
+++ lib/advent_of_code/day_2.ex
@@ -1,39 +1,90 @@
 defmodule AdventOfCode.Day2 do
-  @spec get_frequencies(binary()) :: %{optional(any) => any}
-  def get_frequencies(string) do
-    string
-    |> String.graphemes()
-    |> Enum.reduce(%{}, fn cha, freqs ->
-      curr = freqs[cha] || 0
-      Map.put(freqs, cha, curr + 1)
-    end)
+  defmodule Part1 do
+    @spec get_frequencies(String.t()) :: %{optional(String.t()) => non_neg_integer()}
+    def get_frequencies(string) do
+      string
+      |> String.graphemes()
+      |> Enum.reduce(%{}, fn cha, freqs ->
+        curr = freqs[cha] || 0
+        Map.put(freqs, cha, curr + 1)
+      end)
+    end
+
+    @spec freqs_with_n_repeats(any(), any()) :: non_neg_integer()
+    def freqs_with_n_repeats(freqmaps, n) do
+      freqmaps
+      |> Enum.filter(&Enum.any?(&1, fn {_k, v} -> v == n end))
+      |> length()
+    end
+
+    @spec twos_and_threes(any()) :: [non_neg_integer()]
+    def twos_and_threes(freqmaps) do
+      [freqs_with_n_repeats(freqmaps, 2), freqs_with_n_repeats(freqmaps, 3)]
+    end
+
+    @spec run([String.t()]) :: non_neg_integer()
+    def run(strings) do
+      [twos, threes] =
+        strings
+        |> Enum.map(&get_frequencies(&1))
+        |> twos_and_threes()
+
+      twos * threes
+    end
   end
 
-  @spec freqs_with_n_repeats(any(), any()) :: non_neg_integer()
-  def freqs_with_n_repeats(freqmaps, n) do
-    freqmaps
-    |> Enum.filter(&Enum.any?(&1, fn {_k, v} -> v == n end))
-    |> length()
-  end
+  defmodule Part2 do
+    @spec diff_list(String.t(), String.t()) :: [String.t()]
+    def diff_list(string1, string2) do
+      g1 = String.graphemes(string1)
+      g2 = String.graphemes(string2)
 
-  @spec twos_and_threes(any()) :: [non_neg_integer(), ...]
-  def twos_and_threes(freqmaps) do
-    [freqs_with_n_repeats(freqmaps, 2), freqs_with_n_repeats(freqmaps, 3)]
-  end
+      g1
+      |> Enum.with_index()
+      |> Enum.map(fn {char, idx} ->
+        Enum.at(g2, idx) != char
+      end)
+    end
 
-  @spec part_1() :: non_neg_integer()
-  def part_1 do
-    [twos, threes] =
-      File.read!("input_day_2.txt")
-      |> String.split()
-      |> Enum.map(&get_frequencies(&1))
-      |> twos_and_threes()
+    @spec letters_in_common(String.t(), String.t()) :: String.t()
+    def letters_in_common(str1, str2) do
+      diffs = diff_list(str1, str2)
 
-    twos * threes
+      str1
+      |> String.graphemes()
+      |> Enum.with_index()
+      |> Enum.filter(fn {_char, idx} ->
+        !Enum.at(diffs, idx)
+      end)
+      |> Enum.map(fn {char, _idx} -> char end)
+      |> Enum.join()
+    end
+
+    @spec run([String.t()]) :: String.t()
+    def run(strings) do
+      [[a | [b | []]] | _rest] =
+        strings
+        |> Enum.map(fn str ->
+          matched_other =
+            strings
+            |> Enum.find(fn otherstr ->
+              diff_list(str, otherstr)
+              |> Enum.filter(&(&1 == true))
+              |> length() == 1
+            end)
+
+          [str, matched_other]
+        end)
+        |> Enum.filter(fn [_str, match] -> match end)
+
+      letters_in_common(a, b)
+    end
   end
 
   @spec run() :: :ok
   def run do
-    IO.puts(part_1())
+    strings = File.read!("input_day_2.txt") |> String.split()
+    IO.puts(Part1.run(strings))
+    IO.inspect(Part2.run(strings))
   end
 end
